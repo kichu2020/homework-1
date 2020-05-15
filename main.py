6a18c03429c552aa65ea61cc90e3031bbfd6d6de
kichu2020 <krishnanujam@gmail.com>
2020-05-15 20:10:40 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+print ("hello world")
+print ("49+2="+str(49+2))
+print (49^5)
+print ('''hello world
+hello python''')
+print ("49+2="+str(49+2)+str(49-5))
+print ('''this is a literal in python because it prints everything in the 
+brackets''')
+print (49-6+2-7+12345678909876543212345678909876543234567876543213456)
+print ("this is my homework" )
+print (6**7)
